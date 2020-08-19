a9c11779cf4168381f892a04ec3a4af78eba7a06
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-19 18:05:34 -0400

    Added Review Photos VC file, outlets, setup some UI

---
+++ PrivateYelp/PrivateYelp/ReviewPhotosViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ReviewPhotosViewController.swift
+//  PrivateYelp
+//
+//  Created by Sean Acres on 8/19/20.
+//  Copyright © 2020 Sean Kelson. All rights reserved.
+//
+
+import UIKit
+
+class ReviewPhotosViewController: UIViewController {
+
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpViews()
+    }
+    
+    private func setUpViews() {
+        
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.regular)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.insertSubview(blurEffectView, at: 0)
+        
+        // Set up next button shadows
+        nextButton.layer.shadowColor = UIColor.black.cgColor
+        nextButton.layer.shadowOpacity = 0.25
+        nextButton.layer.shadowRadius = 5
+        nextButton.layer.shadowOffset = CGSize(width: 0, height: 4)
+        nextButton.layer.masksToBounds = false
+        
+        // Set up save button shadows
+        saveButton.layer.shadowColor = UIColor.black.cgColor
+        saveButton.layer.shadowOpacity = 0.25
+        saveButton.layer.shadowRadius = 5
+        saveButton.layer.shadowOffset = CGSize(width: 0, height: 4)
+        saveButton.layer.masksToBounds = false
+        
+        // Set up contentView appearance
+        contentView.layer.cornerRadius = 15.0
+        contentView.layer.shadowColor = UIColor.black.cgColor
+        contentView.layer.shadowOpacity = 0.25
+        contentView.layer.shadowRadius = 5
+        contentView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        contentView.layer.masksToBounds = false
+    }
+    
+
+    @IBAction func addPhotoTapped(_ sender: Any) {
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
