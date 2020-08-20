b90ed13c6585bad3f287fede72a667bd774bdde9
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-20 16:09:43 -0400

    Set up outlets and configure cell look

--- PrivateYelp/PrivateYelp/ReviewTableViewCell.swift
+++ PrivateYelp/PrivateYelp/ReviewTableViewCell.swift
@@ -11,16 +11,31 @@ import UIKit
 class ReviewTableViewCell: UITableViewCell {
 
     @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        updateViews()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    private func updateViews() {
+        cellView.layer.cornerRadius = 15.0
+        
+        //Set up shadows
+        cellView.layer.shadowColor = UIColor.black.cgColor
+        cellView.layer.shadowOpacity = 0.25
+        cellView.layer.shadowRadius = 5
+        cellView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        cellView.layer.masksToBounds = false
     }
     
 }
