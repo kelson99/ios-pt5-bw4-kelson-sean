169fe53c5c70e47c21e127fa580ad383a2304235
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-20 16:07:36 -0400

    Began setting up MyReviews TableVC

---
+++ PrivateYelp/PrivateYelp/ReviewTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ReviewTableViewCell.swift
+//  PrivateYelp
+//
+//  Created by Sean Acres on 8/20/20.
+//  Copyright © 2020 Sean Kelson. All rights reserved.
+//
+
+import UIKit
+
+class ReviewTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
