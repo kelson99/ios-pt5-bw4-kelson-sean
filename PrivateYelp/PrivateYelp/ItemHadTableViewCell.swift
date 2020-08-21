0093976b1398e4c3fb48342d5d9496b5dd06cabb
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-21 11:08:16 -0400

    Continued creating custom table view cell

--- PrivateYelp/PrivateYelp/ItemHadTableViewCell.swift
+++ PrivateYelp/PrivateYelp/ItemHadTableViewCell.swift
@@ -8,8 +8,30 @@
 
 import UIKit
 
-class ItemHadTableViewCell: UITableViewCell {
+protocol ItemHadTableViewCellDelegate: class {
+    func oneStarWasTapped(on cell: ItemHadTableViewCell)
+    func twoStarWasTapped(on cell: ItemHadTableViewCell)
+    func threeStarWasTapped(on cell: ItemHadTableViewCell)
+    func fourStarWasTapped(on cell: ItemHadTableViewCell)
+    func fiveStarWasTapped(on cell: ItemHadTableViewCell)
+}
 
+class ItemHadTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var oneStarButton: UIButton!
+    @IBOutlet weak var twoStarButton: UIButton!
+    @IBOutlet weak var threeStarButton: UIButton!
+    @IBOutlet weak var fourStarButton: UIButton!
+    @IBOutlet weak var fiveStarButton: UIButton!
+    
+    weak var delegate: ItemHadTableViewCellDelegate?
+    var itemHad: String? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -21,4 +43,23 @@ class ItemHadTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    private func updateViews() {
+        guard let itemHad = itemHad else { return }
+        itemNameLabel.text = itemHad
+    }
+    
+    @IBAction func oneStarTapped(_ sender: Any) {
+    }
+    
+    @IBAction func twoStarTapped(_ sender: Any) {
+    }
+    
+    @IBAction func threeStarTapped(_ sender: Any) {
+    }
+    
+    @IBAction func fourStarTapped(_ sender: Any) {
+    }
+    
+    @IBAction func fiveStarTapped(_ sender: Any) {
+    }
 }
